11da909624ae0cccf3f986e66de35f2f80eb6d7f
David6462 <david6462@outlook.com>
2023-10-18 13:39:05 -0500

    Mostrar informacion de usuario en inicio y arreglar ruta de crear gerente

--- src/app/components/Supervisor/supervisor-home/supervisor-home.component.ts
+++ src/app/components/Supervisor/supervisor-home/supervisor-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ApiService } from 'src/app/services/api/api.service';
+import { LocalDataService } from 'src/app/services/localData/local-data.service';
 
 @Component({
   selector: 'app-supervisor-home',
@@ -8,12 +10,31 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class SupervisorHomeComponent implements OnInit {
   safeURL: any;
+  zona_asignada = "";
+  municipio_asignado = "";
+  departamento_asignado = "";
 
-  constructor(private _sanitizer: DomSanitizer) {
+  constructor(private _sanitizer: DomSanitizer, private apiService: ApiService, private localData: LocalDataService) {
     this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(
       'https://www.youtube.com/embed/bNU_d8rei4k'
     );
   }
 
-  ngOnInit(): void {}
+  ngOnInit(){
+    this.getSupervisor();
+  }
+
+  getSupervisor(){
+    this.apiService.getCoordinador(this.localData.getId()).subscribe((resp: any) => {
+
+      console.log(resp);
+
+      this.zona_asignada = resp.zonas_asignadas.nombre;
+
+      this.municipio_asignado = resp.municipio.nombre;
+
+      this.departamento_asignado = resp.departamento.nombre_departamento_votacion;
+
+    })
+  }
 }
